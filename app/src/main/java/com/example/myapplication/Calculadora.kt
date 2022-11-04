4046635afd6d3b11026fb931583f5e7307a2d745
Andre Furlan <ensismoebius@gmail.com>
2022-11-04 11:18:13 -0300

    Reinicio

--- app/src/main/java/com/example/myapplication/Calculadora.kt
+++ app/src/main/java/com/example/myapplication/Calculadora.kt
@@ -2,10 +2,23 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class Calculadora : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_calculadora)
+
+
+        val igual: Button = findViewById(R.id.btnIgual)
+        igual.setOnClickListener {
+            val avaliador : EvaluateString = EvaluateString();
+            val txtformula : TextView = this.findViewById(R.id.txtFormula);
+            val expr: String = txtformula.text.toString()
+            txtformula.text = avaliador.evaluate(expr)
+        }
+
+
     }
 }
