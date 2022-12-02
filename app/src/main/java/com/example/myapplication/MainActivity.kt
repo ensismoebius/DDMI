15a0a4cb7bc42be471d273e5c8e77219bef439a4
Andre Furlan <ensismoebius@gmail.com>
2022-12-02 10:28:57 -0300

    Reinicio

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -4,13 +4,14 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val btnCalculadora: Button = findViewById(R.id.btnCalculadora)
+        val btnCalculadora: ImageButton = findViewById(R.id.btnCalculadora)
         btnCalculadora.setOnClickListener{
             abrirCalculadora()
         }
